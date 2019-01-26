69641050d1f01412f484ac6d507d73856b510438
Timothy Clarkson <clarksontim@outlook.com>
2019-01-26 15:32:10 +0000

    Created Entities +Created the shopAdjustments and shopAdjustmentLine entities +Created the shopDeliveries and shopDeliveryLines entities +Created the shopReturns and shopReturnLines entities +Created the shopTransfers and shopTransferLines entities

---
+++ StockManager.Data/Data/Entities/shopAdjustments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockManager.Data.Data.Entities
+{
+    public class shopAdjustments
+    {
+        [Key]
+        public int ShopAdjustID { get; set; }
+        StringLength(8)]
+        public string ShopRef { get; set; }
+        public string Reference { get; set; }
+        public int TotalLossItems { get; set; }
+        public int TotalGainItems { get; set; }
+        [DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime MovementDate { get; set; }
+        [StringLength(16)]
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ StockManager.Data/Data/Entities/shopReturnLines.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockManager.Data.Data.Entities
+{
+    public class shopReturnLines
+    {
+        [Key]
+        [Required]
+        public int ReturnLineID { get; set; }
+        public int ReturnID { get; set; }
+        public string StockCode { get; set; }
+        public int Qty { get; set; }
+        public double Value { get; set; }
+    }
+}
