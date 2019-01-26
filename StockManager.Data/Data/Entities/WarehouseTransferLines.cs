2a7e106566a6f709697d46fefd8dafab0a423281
DarrenHannay2019 <dmhsoftad01@outlook.com>
2019-01-26 15:33:12 +0000

    Created Entities PurchaseOders, WarehouseAdjustments, Warehouse Returns and Warehouse Transfers

---
+++ StockManager.Data/Data/Entities/WarehouseTransferLines.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockManager.Data.Data.Entities
+{
+    public class WarehouseTransferLines
+    {
+        [Key]
+        public int WarehouseTransferLineID { get; set; }
+        public int WarehouseTransferID { get; set; }
+        [StringLength(30)]
+        public string StockCode { get; set; }        
+        public int TOQtyGarments { get; set; }      
+        public int TOQtyBoxes { get; set; }       
+        public int TOQtyUnits { get; set; }      
+    }
+}
