b91db5ceccca92391d34a999c78603cbb3cc28d1
DarrenHannay2019 <dmhsoftad01@outlook.com>
2019-01-26 14:16:07 +0000

    Start of Project Added all functionality for spliting the atom bomb in that all data is sperate from the user interface

---
+++ StockManager.Data/Data/SMContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using StockManager.Data.Data.Entities;
+
+namespace StockManager.Data.Data
+{
+    public class SMContext : IdentityDbContext<User>
+    {
+        public SMContext(DbContextOptions<SMContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
