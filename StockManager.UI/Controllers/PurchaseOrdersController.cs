1763dcbfdb52bc08a908d13236b1d138a62c0372
DarrenHannay2019 <dmhsoftad01@outlook.com>
2019-01-28 06:34:31 +0000

    Added Warehouse subfunctions and styled Shop's

---
+++ StockManager.UI/Controllers/PurchaseOrdersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StockManager.Data.Data;
+using StockManager.Data.Data.Entities;
+
+namespace StockManager.UI.Controllers
+{
+    public class PurchaseOrdersController : Controller
+    {
+        private readonly SMContext _context;
+
+        public PurchaseOrdersController(SMContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PurchaseOrders
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.PurchaseOrder.ToListAsync());
+        }
+
+        // GET: PurchaseOrders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var deliveries = await _context.PurchaseOrder
+                .SingleOrDefaultAsync(m => m.DeliveryID == id);
+            if (deliveries == null)
+            {
+                return NotFound();
+            }
+
+            return View(deliveries);
+        }
+
+        // GET: PurchaseOrders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PurchaseOrders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DeliveryID,StockCode,SupplierRef,WarehouseRef,Season,TotalGarments,TotalHangers,TotalBoxes,NetAmount,DeliveryCharge,Commission,VATAmount,GrossAmount,DeliveryDate,Notes,Invoice,Shipper,ShipperInvoice,CreatedBy,CreatedDate")] Deliveries deliveries)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(deliveries);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(deliveries);
+        }
+
+        // GET: PurchaseOrders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var deliveries = await _context.PurchaseOrder.SingleOrDefaultAsync(m => m.DeliveryID == id);
+            if (deliveries == null)
+            {
+                return NotFound();
+            }
+            return View(deliveries);
+        }
+
+        // POST: PurchaseOrders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DeliveryID,StockCode,SupplierRef,WarehouseRef,Season,TotalGarments,TotalHangers,TotalBoxes,NetAmount,DeliveryCharge,Commission,VATAmount,GrossAmount,DeliveryDate,Notes,Invoice,Shipper,ShipperInvoice,CreatedBy,CreatedDate")] Deliveries deliveries)
+        {
+            if (id != deliveries.DeliveryID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(deliveries);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DeliveriesExists(deliveries.DeliveryID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(deliveries);
+        }
+
+        // GET: PurchaseOrders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var deliveries = await _context.PurchaseOrder
+                .SingleOrDefaultAsync(m => m.DeliveryID == id);
+            if (deliveries == null)
+            {
+                return NotFound();
+            }
+
+            return View(deliveries);
+        }
+
+        // POST: PurchaseOrders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var deliveries = await _context.PurchaseOrder.SingleOrDefaultAsync(m => m.DeliveryID == id);
+            _context.PurchaseOrder.Remove(deliveries);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DeliveriesExists(int id)
+        {
+            return _context.PurchaseOrder.Any(e => e.DeliveryID == id);
+        }
+    }
+}
---
+++ StockManager.UI/Controllers/WarehouseAdjustmentsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StockManager.Data.Data;
+using StockManager.Data.Data.Entities;
+
+namespace StockManager.UI.Controllers
+{
+    public class WarehouseAdjustmentsController : Controller
+    {
+        private readonly SMContext _context;
+
+        public WarehouseAdjustmentsController(SMContext context)
+        {
+            _context = context;
+        }
+
+        // GET: WarehouseAdjustments
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.WarehouseAdjustment.ToListAsync());
+        }
+
+        // GET: WarehouseAdjustments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouseAdjustments = await _context.WarehouseAdjustment
+                .SingleOrDefaultAsync(m => m.WarehouseAdjustID == id);
+            if (warehouseAdjustments == null)
+            {
+                return NotFound();
+            }
+
+            return View(warehouseAdjustments);
+        }
+
+        // GET: WarehouseAdjustments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WarehouseAdjustments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WarehouseAdjustID,WarehouseRef,Reference,StockCode,MovementType,TotalItems,MovementDate,CreatedBy,CreatedDate")] WarehouseAdjustments warehouseAdjustments)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(warehouseAdjustments);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(warehouseAdjustments);
+        }
+
+        // GET: WarehouseAdjustments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouseAdjustments = await _context.WarehouseAdjustment.SingleOrDefaultAsync(m => m.WarehouseAdjustID == id);
+            if (warehouseAdjustments == null)
+            {
+                return NotFound();
+            }
+            return View(warehouseAdjustments);
+        }
+
+        // POST: WarehouseAdjustments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WarehouseAdjustID,WarehouseRef,Reference,StockCode,MovementType,TotalItems,MovementDate,CreatedBy,CreatedDate")] WarehouseAdjustments warehouseAdjustments)
+        {
+            if (id != warehouseAdjustments.WarehouseAdjustID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(warehouseAdjustments);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WarehouseAdjustmentsExists(warehouseAdjustments.WarehouseAdjustID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(warehouseAdjustments);
+        }
+
+        // GET: WarehouseAdjustments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouseAdjustments = await _context.WarehouseAdjustment
+                .SingleOrDefaultAsync(m => m.WarehouseAdjustID == id);
+            if (warehouseAdjustments == null)
+            {
+                return NotFound();
+            }
+
+            return View(warehouseAdjustments);
+        }
+
+        // POST: WarehouseAdjustments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var warehouseAdjustments = await _context.WarehouseAdjustment.SingleOrDefaultAsync(m => m.WarehouseAdjustID == id);
+            _context.WarehouseAdjustment.Remove(warehouseAdjustments);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WarehouseAdjustmentsExists(int id)
+        {
+            return _context.WarehouseAdjustment.Any(e => e.WarehouseAdjustID == id);
+        }
+    }
+}
